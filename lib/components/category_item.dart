5d78c38d3c84483ef94fa4f8b968c68079bdbe89
Fulvio Leo <fulviokaldas@outlook.com>
2022-04-18 14:36:00 -0300

    added the navigation to a category page when clicking a category

--- lib/components/category_item.dart
+++ lib/components/category_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:project_meals/screens/categories_meals_screen.dart';
 
 import '../models/category.dart';
 
@@ -9,23 +10,38 @@ class CategoryItem extends StatelessWidget {
     this.category,
   });
 
+  void _selectCategory(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (_) {
+          return const CategoriesMealsScreen();
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(15),
-      child: Text(
-        category!.title!,
-        style: Theme.of(context).textTheme.headline6,
-      ),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(15),
-        gradient: LinearGradient(
-          colors: [
-            category!.color!.withOpacity(0.5),
-            category!.color!,
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+    return InkWell(
+      onTap: () => _selectCategory(context),
+      borderRadius: BorderRadius.circular(15),
+      splashColor: Theme.of(context).colorScheme.primary,
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        child: Text(
+          category!.title!,
+          style: Theme.of(context).textTheme.headline6,
+        ),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          gradient: LinearGradient(
+            colors: [
+              category!.color!.withOpacity(0.5),
+              category!.color!,
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
         ),
       ),
     );
